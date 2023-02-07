b57a30485b31af822e8f110df9a87b2591082697
xcr4t3 <67573485+xcr4t3@users.noreply.github.com>
2023-02-07 01:58:30 -0300

    Easter egg

--- lib/_math.py
+++ lib/_math.py
@@ -12,8 +12,9 @@ def _mul(alpha, bravo, var: str):
         VAR._def(var, (alpha*bravo))
 def _div(alpha, bravo, var: str):
     if (type(alpha) == float or type(alpha) == int) and (type(bravo) == float or type(bravo) == int):
-        if bravo == 0 or alpha == 0:
-            VAR._def(var, 0)
+        if bravo == 0:
+            print("Can't divide by 0 unless you are the math's creator")
+            exit()
         else:
             VAR._def(var, (alpha/bravo))
 def _ele(alpha, bravo, var: str):
